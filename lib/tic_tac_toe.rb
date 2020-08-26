6a721a86da6e48283f11a57011ba868a8cfa83ba
Alma Flores <flores.almalee@gmail.com>
2020-08-26 19:10:37 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,23 @@
+class TicTacToe 
+  def initialize
+    @board = Array.new(9, " ")
+  end 
+  
+  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  
+  def display_board 
+    self.board
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  def move(index, current_player = 'X')
+    @board[index] = current_player
+  end
+  
+  def position_taken?(index)
+    @board[index].nil? || @board[index] == " " ? false : true
+  end
+end
