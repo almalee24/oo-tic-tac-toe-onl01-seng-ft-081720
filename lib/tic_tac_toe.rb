a2dd3f21c402f2dc09cd9fa6621441541bc7ac68
Alma Flores <flores.almalee@gmail.com>
2020-08-26 20:32:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,11 @@ class TicTacToe
   
   def turn_count 
     turn = 0 
-    @board.each{|index| index == "X" || index == "O" ? turn += 1 : } 
+    @board.each do |index|
+      if index == "X" || index == "O"
+        turn += 1
+      end 
+    end 
     turn
   end
   
