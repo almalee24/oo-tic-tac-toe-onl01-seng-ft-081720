1cba5ffac602bf674d58145205acc37004ab12be
Alma Flores <flores.almalee@gmail.com>
2020-08-26 20:34:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,12 +63,12 @@ class TicTacToe
       position_3 = @board[winner_combo[2]] 
       
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return winner_combo
+        winner_combo
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return winner_combo
+        winner_combo
       end
     }
-    return false
+    false
   end
   
   def full?
